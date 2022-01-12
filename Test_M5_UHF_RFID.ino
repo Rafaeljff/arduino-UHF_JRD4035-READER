fbd48e978722d95d6547f90419422863a6a83809
Rafaeljff <45770575+Rafaeljff@users.noreply.github.com>
2022-01-12 16:56:40 +0000

    Add files via upload

--- Test_M5_UHF_RFID.ino
+++ Test_M5_UHF_RFID.ino
@@ -16,7 +16,7 @@ TestInfo Test;
 void setup() {
 	Serial.begin(115200);
 	RFID._debug = 0;
-	Serial.begin(115200, SERIAL_8N1, 16, 17); //16.17
+	Serial2.begin(115200, SERIAL_8N1, 16, 17); //16.17
 if (RFID._debug == 1){
 	//Serial.begin(115200, SERIAL_8N1, 21, 22);
   Serial.println("Using Uart CHANNEL 0");
@@ -27,7 +27,7 @@ if (RFID._debug == 1){
 	RFID.Readcallback();
 	RFID.clean_data();
 
-	Serial.println("debuggggggg");
+
 
 // Determined whether to connect to UHF_RFID 判断是否连接UHF_RFID
 	/* String soft_version;
@@ -43,23 +43,30 @@ if (RFID._debug == 1){
 
 void loop() {
 
-	RFID.Delay(100);
+	//card = RFID.A_single_poll_of_instructions();
+	cards=RFID.Multiple_polling_instructions(10);
+
+     //Serial.println("RSSI :" + cards.card[1]._RSSI);
+    // Serial.println("PC :" + cards.card[1]._PC);
+     //Serial.println("EPC :" + cards.card[1]._EPC);
+     ///kSerial.println(" ");
+
+     for (size_t i = 0; i < cards.len; i++)
+      {
+       if(cards.card[i]._EPC.length() == 24)
+          {
+           Serial.println("RSSI :" + cards.card[i]._RSSI);
+           Serial.println("PC :" + cards.card[i]._PC);
+           Serial.println("EPC :" + cards.card[i]._EPC);
+           Serial.println("CRC :" + cards.card[i]._CRC);
+           Serial.println(" ");
+           Serial.println(" ");
+          }
+      }
 
 
-	card = RFID.A_single_poll_of_instructions();
-	card2=RFID.Read_working_area();
-	Serial.println(card2.Region);
-	Serial.println(card2.Mixer_G);
-	Serial.println(card2.IF_G);
-	Serial.println("Debug");
-	if (card._ERROR.length() != 0) {
-		Serial.println(card._ERROR);
-	}
-else{
 
-
-
-
+//read a single tag
 	if(card._EPC.length() == 24){
      Serial.println("RSSI :" + card._RSSI);
      Serial.println("PC :" + card._PC);
@@ -68,10 +75,78 @@ else{
      Serial.println(" ");
 
    }
- }
 
+	 /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+	       Used to get the SELECT parameter 用于获取Select参数
+	       ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+	     //  Select = RFID.Get_the_select_parameter();
+	     //  if(Select.Mask.length() != 0)
+	     //  {
+	     //    canvas.println("Mask :" + Select.Mask);
+	     //    canvas.println("SelParam :" + Select.SelParam);
+	     //    canvas.println("Ptr :" + Select.Ptr);
+	     //    canvas.println("MaskLen :" + Select.MaskLen);
+	     //    canvas.println("Truncate :" + Select.Truncate);
+	     //    canvas.println(" ");
+	     //  }
+	     //    RFID.clean_data();
+
+	     /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+	       Used to change the PSF bit of the NXP G2X label 用于改变 NXP G2X 标签的 PSF 位
+	       ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+	     //  Cardinformation = RFID.NXP_Change_EAS(0x00000000);
+	     //  if(Cardinformation._UL.length() != 0)
+	     //  {
+	     //    canvas.println("UL :" + Cardinformation._UL);
+	     //    canvas.println("PC :" + Cardinformation._PC);
+	     //    canvas.println("EPC :" + Cardinformation._EPC);
+	     //    canvas.println("Parameter :" + Cardinformation._Parameter);
+	     //    canvas.println("ErrorCode :" + Cardinformation._ErrorCode);
+	     //    canvas.println("Error :" + Cardinformation._Error);
+	     //    canvas.println("Data :" + Cardinformation._Data);
+	     //    canvas.println("Successful :" + Cardinformation._Successful);
+	     //    canvas.println(" ");
+	     //   }
+	     //    RFID.clean_data();
+
+
+	     /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+	       Used to get the Query parameters 用于获取Query参数
+	       ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+	     //  Query = RFID.Get_the_Query_parameter();
+	     //  if(Query.QueryParameter.length() != 0)
+	     //  {
+	     //    canvas.println("QueryParameter :" + Query.QueryParameter);
+	     //    canvas.println("DR :" + Query.DR);
+	     //    canvas.println("M :" + Query.M);
+	     //    canvas.println("TRext :" + Query.TRext);
+	     //    canvas.println("Sel :" + Query.Sel);
+	     //    canvas.println("Session :" + Query.Session);
+	     //    canvas.println("Targetta :" + Query.Target);
+	     //    canvas.println("Q :" + Query.Q);
+	     //    canvas.println(" ");
+	     //  }
+	     //  RFID.clean_data();
+
+
+	     /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+	       Used to get the Query parameters 用于读取接收解调器参数
+	       ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+	     //  Read = RFID.Read_receive_demodulator_parameters();
+	     //  if(Read.Mixer_G.length()!= 0)
+	     //  {
+	     //    canvas.println("Mixer_G :" + Read.Mixer_G);
+	     //    canvas.println("IF_G :" + Read.IF_G);
+	     //    canvas.println("Thrd :" + Read.Thrd);
+	     //    canvas.println(" ");
+	     //  }
 
 	RFID.clean_data();
 
 	delay(1000);
+
+
+
+
 }
+
