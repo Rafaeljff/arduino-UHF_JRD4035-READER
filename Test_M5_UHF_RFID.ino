dbfcb21c331e3e970696637f8f15c92be8d52b5e
Rafaeljff <45770575+Rafaeljff@users.noreply.github.com>
2022-01-24 11:09:16 +0000

    Add files via upload

--- Test_M5_UHF_RFID.ino
+++ Test_M5_UHF_RFID.ino
@@ -1,6 +1,9 @@
 #include "Arduino.h"
 #include "RFID_command.h"
+#include <Ds1302.h>
+#define OFFSETOF(TYPE, ELEMENT) ((size_t)&(((TYPE *)0)->ELEMENT))
 
+//UHF_JRD3045
 UHF_RFID RFID;
 
 String comd = " ";
@@ -13,65 +16,95 @@ QueryInfo Query;
 ReadInfo Read;
 TestInfo Test;
 
+// DS1302 RTC instance
+Ds1302 rtc(5, 22, 21);
+const static char *WeekDays[] = { "Monday", "Tuesday", "Wednesday", "Thursday",
+		"Friday", "Saturday", "Sunday" };
+// DS1302 RTC instance
+
+typedef struct check_box {
+	String tag_count[10];
+
+} check_box_tags;
+check_box_tags checkbox_set[30];
+const int button_read = 27;
 void setup() {
+
 	Serial.begin(115200);
-	RFID._debug = 0;
-	Serial.begin(115200, SERIAL_8N1, 16, 17); //16.17
-if (RFID._debug == 1){
-	//Serial.begin(115200, SERIAL_8N1, 21, 22);
-  Serial.println("Using Uart CHANNEL 0");
-}
+
+	Serial2.begin(115200, SERIAL_8N1, 16, 17); //16.17
+	rtc.init();
+	attachInterrupt(button_read, func_read_all, FALLING);
 	RFID.Set_transmission_Power(2600);
 	RFID.Set_the_Select_mode();
 	RFID.Delay(100);
 	RFID.Readcallback();
 	RFID.clean_data();
 
-	Serial.println("debuggggggg");
-
 // Determined whether to connect to UHF_RFID 判断是否连接UHF_RFID
 	/* String soft_version;
-	soft_version = RFID.Query_software_version();
+	 soft_version = RFID.Query_software_version();
 	 { while(soft_version.indexOf("V2.3.5") == -1)
-	  {
+	 {
 	 RFID.clean_data();
 	 RFID.Delay(150);
 	 soft_version = RFID.Query_software_version();
 	 }
-	Serial.println("Please approach the RFID card you need to use");
-*/}
+	 Serial.println("Please approach the RFID card you need to use");
+	 */}
 
 void loop() {
 
-	RFID.Delay(100);
+	//card = RFID.A_single_poll_of_instructions();
+	Ds1302::DateTime now;
+	rtc.getDateTime(&now);
+
+	//Cardinformation = RFID.NXP_Change_EAS(0x00000000);
+	//Serial.println("RSSI :" + cards.card[1]._RSSI);
+	// Serial.println("PC :" + cards.card[1]._PC);
+	//Serial.println("EPC :" + cards.card[1]._EPC);
+	///kSerial.println(" ");
+	 func_read_all();
+	rtc.getDateTime(&now);
+	 Serial.println("\t");
+	 Serial.print(now.year);    // 00-99
+	 Serial.print('-');
+
+	 Serial.print(now.month);   // 01-12
+	 Serial.print('-');
+	 Serial.print(now.day);     // 01-31
+	 Serial.print(' ');
+	 Serial.print(WeekDays[now.dow - 1]);
+	 Serial.print(' ');
+	 Serial.print(now.hour);    // 00-23
+	 Serial.print(':');
+	 Serial.print(now.minute);  // 00-59
 
 
-	card = RFID.A_single_poll_of_instructions();
-	card2=RFID.Read_working_area();
-	Serial.println(card2.Region);
-	Serial.println(card2.Mixer_G);
-	Serial.println(card2.IF_G);
-	Serial.println("Debug");
-	if (card._ERROR.length() != 0) {
-		Serial.println(card._ERROR);
-	}
-else{
 
 
+	 delay(5000);
 
-
-	if(card._EPC.length() == 24){
-     Serial.println("RSSI :" + card._RSSI);
-     Serial.println("PC :" + card._PC);
-     Serial.println("EPC :" + card._EPC);
-     Serial.println("CRC :" + card._CRC);
-     Serial.println(" ");
-
-   }
- }
-
-
-	RFID.clean_data();
-
-	delay(1000);
+	 //delay(1000);
 }
+void func_read_all(void) {
+
+	cards = RFID.Multiple_polling_instructions(10);
+	for (size_t i = 0; i < cards.len; i++) {
+		Serial.print("\n");
+		if (cards.card[i]._EPC.length() == 24) {
+
+			checkbox_set[0].tag_count[i] = cards.card[i]._EPC;
+			Serial.print("\t tag");
+			Serial.print(i);
+			Serial.print(":");
+			Serial.print(checkbox_set[0].tag_count[i]);    // 00-99
+
+		}
+
+	}
+
+RFID.clean_data();
+}
+
+
